6add8562020db9f5c9cd5a97307772cef872b1f9
nagelricardo <ricardo.nagel@hotmail.com>
2022-02-20 16:11:27 -0300

    Encurtador links(Layout, componentes e rotas)

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import './menu.css';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+
+export default function Menu(){
+    return(
+        <div className='menu'>
+            <a className='social' href="https://github.com/nagelricardo">
+            <BsGithub size={24} color='#fff'/> 
+            </a>
+            
+            <a className='social' href="https://www.linkedin.com/in/ricardonagel/">
+            <BsLinkedin size={24} color='#fff'/> 
+            </a>
+            
+            <Link className='menu-item' to="/links">
+                Meus Links
+            </Link>
+        </div>
+    )   
+}
---
+++ src/pages/Links/index.js
@@ -0,0 +1,28 @@
+import './links.css';
+import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+export default function Links() {
+    return (
+      <div className='links-container'>
+
+        <div className='links-header'>
+           <Link to="/">
+            <FiArrowLeft size={38} color='#fff'/>
+           </Link>
+           <h1>Meus Links</h1>
+        </div>
+
+        <div className='links-item'>
+           <button className='link'>
+             <FiLink size={18} color='#fff'/>
+             https://google.com
+           </button>
+           <button className='link-delete'> 
+             <FiTrash size={24} color='#FF5454'/>
+           </button>
+        </div>
+
+      </div>
+    )
+  }
